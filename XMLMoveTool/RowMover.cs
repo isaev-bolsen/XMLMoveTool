6618f1d04c272bf6774e8813174df95bfac2eaab
isaev-bolsen <al_mamistov@mail.ru>
2017-10-24 20:27:16 +0300

    Add RowMover

---
+++ XMLMoveTool/RowMover.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Linq;
+
+namespace XMLMoveTool
+{
+    public static class RowMover 
+    {
+        public static void Move (XDocument source, XDocument destination)
+        {
+
+        }
+    }
+}
