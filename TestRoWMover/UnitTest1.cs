485155bb9355142038dbdca4cda32812fd722665
isaev-bolsen <al_mamistov@mail.ru>
2017-10-24 20:42:32 +0300

    The test

---
+++ TestRoWMover/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace TestRoWMover
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRowMover()
+        {
+            XDocument source = new XDocument(new XElement("data",
+                new XElement("table"),
+                new XElement("rows", new XElement("row"))
+                ));
+            XDocument destination = new XDocument(new XElement("data",
+                new XElement("table"),
+                new XElement("rows")
+                ));
+
+            XMLMoveTool.RowMover.Move(source, destination);
+            XElement resultingRow = destination.Descendants("rows").Single();
+            Assert.AreEqual(1, resultingRow.Elements().Count());
+        }
+    }
+}
